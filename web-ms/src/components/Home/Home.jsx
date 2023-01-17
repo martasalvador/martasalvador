a9ba2ca22aa2bc88c4998dceca1311155b2f70e6
Marta Salvador <martasbsv@gmail.com>
2023-01-17 17:04:12 +0100

    update

--- web-ms/src/components/Home/Home.jsx
+++ web-ms/src/components/Home/Home.jsx
@@ -5,29 +5,35 @@ const Home = () => {
         <>
         <section className='firstSection'>
             <div>
-                <h1 className='titles'>Full Stack</h1>
-                <h1 className='titles'>Developer</h1>
+                <h1 className='devTitle'>Full Stack</h1>
+                <h1 className='devTitle'>Developer</h1>
             </div>
             <div>
-                <img className='uxuiImage' src="https://images.unsplash.com/photo-1618477388954-7852f32655ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="firstImage" />
+                <img className='devImage' src="https://images.unsplash.com/photo-1618477388954-7852f32655ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="firstImage" />
             </div>
-            <div className='thirdcolumn'>
-                <img className='eyeIcon' src="https://res.cloudinary.com/dcycbjbne/image/upload/v1673534893/portfolio/eye-icon_bs5cnv.png" alt="secondImage" />
-                <p className='text'><em>'Design is everywhere.</em></p> 
-                <p><em>From the dress you’re wearing to the smartphone you’re holding.'</em></p>
-                <hr className='hr' />
-                </div>
+            
         </section>
             
             <h1>&</h1>
 
         <section className='secondSection'>
-            <div>
-                <img src="" alt="thirdImage" />
+            <div className='imageContainer'>
+                    <img className='uxuiImage1' src="https://images.unsplash.com/photo-1618477388954-7852f32655ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="firstImage" />
+                    <br />
+                    <img className='uxuiImage2' src="https://images.unsplash.com/photo-1618477388954-7852f32655ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="firstImage" />
                 <p>Text</p>
             </div>
             <div>
-                <h1 className='titles'>UX/UI Designer</h1>
+                <h1 className='uxuiTitle'>UX/UI Designer</h1>
+                <div className='thirdcolumn'>
+                    <img className='eyeIcon' src="https://res.cloudinary.com/dcycbjbne/image/upload/v1673534893/portfolio/eye-icon_bs5cnv.png" alt="secondImage" />
+                    <p className='designText'><em>Design Thinking</em></p> 
+                    <p className='designText'><em>Atomic Design</em></p> 
+                    <p className='designText'><em>Design Sprint</em></p> 
+                    <p className='designText'><em>High & Low Fidelity</em></p> 
+                    
+                    <hr className='hr' />
+                </div>
             </div>
         </section>
         </>
